789ddb54b41a84d29a9994c1458006481d301194
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-08-14 10:58:33 -0700

    Add tests and stuff

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# split_slice
-A Rust library for accessing (&amp;[T], &amp;[T]) as if it were a single slice
+# `split_slice`
+[![Crates.io](https://img.shields.io/crates/v/split_slice.svg)](https://crates.io/crates/split_slice)
+[![Docs.rs](https://img.shields.io/docsrs/split_slice)](https://docs.rs/split_slice)
+
+A Rust library for accessing `(&[T], &[T])` as if it were a single slice.
+
+## Features
+- Very simple to construct
+- `len` and `is_empty`
+- `impl Debug` shows it as a single list
+- `Iterator`
+- Pure Rust, no dependencies, works with `no_std`
+- No `alloc` used
+- `T` does not need to implement `Clone` or `Copy`
+
+## Use Cases
+- Ring buffers
+- Custom `VecDequeue`
---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+#![no_std]
+
+use core::fmt::Debug;
+
+#[derive(Copy)]
+pub struct SplitSlice<'a, T>(pub &'a [T], pub &'a [T]);
+
+impl<T> SplitSlice<'_, T> {
+    pub fn len(&self) -> usize {
+        self.0.len() + self.1.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty() && self.1.is_empty()
+    }
+}
+
+impl<T> Clone for SplitSlice<'_, T> {
+    fn clone(&self) -> Self {
+        Self(self.0, self.1)
+    }
+}
+
+impl<T: Debug> Debug for SplitSlice<'_, T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_list().entries(self.clone().into_iter()).finish()
+    }
+}
+
+impl<'a, T> IntoIterator for SplitSlice<'a, T> {
+    type IntoIter = SplitSliceIterator<'a, T>;
+    type Item = &'a T;
+
+    fn into_iter(self) -> Self::IntoIter {
+        SplitSliceIterator::new(self)
+    }
+}
+
+pub struct SplitSliceIterator<'a, T> {
+    split_slice: SplitSlice<'a, T>,
+    position: usize,
+}
+
+impl<'a, T> SplitSliceIterator<'a, T> {
+    fn new(split_slice: SplitSlice<'a, T>) -> Self {
+        Self {
+            split_slice,
+            position: 0,
+        }
+    }
+}
+
+impl<'a, T> Iterator for SplitSliceIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.position < self.split_slice.0.len() {
+            let item = &self.split_slice.0[self.position];
+            self.position += 1;
+            Some(item)
+        } else {
+            let position_2 = self.position - self.split_slice.0.len();
+            let item = self.split_slice.1.get(position_2)?;
+            self.position += 1;
+            Some(item)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
+}
+
+impl<T> ExactSizeIterator for SplitSliceIterator<'_, T> {
+    fn len(&self) -> usize {
+        self.split_slice.len() - self.position
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iterate() {
+        assert!(
+            SplitSlice(&[1, 2, 3], &[4, 5])
+                .into_iter()
+                .copied()
+                .eq([1, 2, 3, 4, 5])
+        );
+    }
+
+    #[test]
+    fn iterate_first_empty() {
+        assert!(SplitSlice(&[], &[1, 2]).into_iter().copied().eq([1, 2]));
+    }
+
+    #[test]
+    fn iterate_second_empty() {
+        assert!(SplitSlice(&[1, 2], &[]).into_iter().copied().eq([1, 2]));
+    }
+
+    #[test]
+    fn iterate_both_empty() {
+        assert!(SplitSlice::<i32>(&[], &[]).into_iter().copied().eq([]));
+    }
+
+    #[test]
+    fn len() {
+        assert_eq!(SplitSlice(&[1, 2, 3, 4], &[5]).len(), 5);
+    }
+}
